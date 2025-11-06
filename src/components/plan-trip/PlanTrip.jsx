faff7533eb0e0959f5a9140adb6dd670f9b86f68
alexmadaras00 <alexmad56.am@gmail.com>
2025-11-06 17:13:43 +0200

    MyProfile Design Completed + BugFix - Travel Preferences is now working properly - Fixed the list adding elements in preferences - Fixed the sessionStorage variables, by changing them to localStorage - Refactored the code in TripSummary, by splitting tabs into separate components - MyTrips is again a separate page - Back button on PlanTrip.jsx fixed

--- src/components/plan-trip/PlanTrip.jsx
+++ src/components/plan-trip/PlanTrip.jsx
@@ -125,8 +125,7 @@ export default function PlanTrip() {
                         <div className="bottom-plan-trip-container">
                             <button
                                 className="btn btn-secondary"
-                                onClick={() => setCurrentStep(Math.max(1, currentStep - 1))}
-                                disabled={currentStep === 1}
+                                onClick={() =>  currentStep>1 ? setCurrentStep(Math.max(1, currentStep - 1)) : navigate("/")}
                             >
                                 Back
                             </button>
